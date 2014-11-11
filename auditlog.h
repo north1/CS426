f13f06bac39b3d9ca139efe6d8e1a673572917f9
north1 <north1@purdue.edu>
2014-11-11 01:10:00 -0500

    added entry struct to auditlog.h, since it's going to be used everywhere

--- auditlog.h
+++ auditlog.h
@@ -21,6 +21,13 @@ struct log {
     int Ao; //random starting point
 };
 
+struct entry {
+	int type; //entry type, possibly unnecessary, subject to change
+	char encryptedMessage[100] //arbitrary size, may change?
+	char hashChain[20] //note: initial entry should have hashChain of all zeros
+	char integrity[20] //= MAC_Aj(hashChain) -- definitely subject to change since I have no goddamn idea what MAC is
+};
+
 int logfileInitType = 0; 
 int responseMessageType = 1; 
 //int abnormalCloseType = 2; never happens because timeouts can't happen (TA is cool with this)
